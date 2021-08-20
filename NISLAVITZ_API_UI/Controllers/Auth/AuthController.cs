70a30ff0fa0a6d4ad3b4300d261686ddb56dd86e
Andrzej Bronisławski <eazymen789@gmail.com>
2021-08-20 22:13:08 +0200

    Added method skeletons to AuthController

--- NISLAVITZ_API_UI/Controllers/Auth/AuthController.cs
+++ NISLAVITZ_API_UI/Controllers/Auth/AuthController.cs
@@ -1,6 +1,24 @@
-﻿namespace NISLAVITZ_API_UI.Controllers
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NISLAVITZ_API_UI.Controllers
 {
-	public class AuthController
+	[ApiController]
+	[Produces("application/json")]
+	public class AuthController : ControllerBase
 	{
+		[HttpPost]
+		[Route("GetToken")]
+		public async Task<ActionResult<bool>> GetToken()
+		{
+			return Ok(new { Success = true });
+		}
+
+		[HttpGet]
+		[Route("RevokeToken")]
+		public async Task<ActionResult<bool>> RevokeToken()
+		{
+			return Ok(new { Success = true });
+		}
 	}
 }
